6e521872f5e194f47c6c4b76c38c49a9b48d1548
bjornops <bjornops@gmail.com>
2018-05-30 12:12:38 +0200

    Generate average reward for experiment id's in experiment folder

--- mains/statistics_generator.py
+++ mains/statistics_generator.py
@@ -1,10 +1,20 @@
 import numpy as np
 import tensorboard.backend.event_processing.event_accumulator as event_accumulator
+import glob, os
 
 def main():
-    event_file_path = ""
-    event_avg = calc_event_reward_avg(event_file_path)
-    print(str(event_avg))
+    exp_name = "cold_env_reset"
+    exp_dir = summary_path(exp_name)
+
+    print("Generating statistics for experiment '" + exp_name + "'")
+    print("From directory: '" + exp_dir + "'")
+
+    event_dirs = get_event_dirs(exp_dir)
+    for dir in event_dirs:
+        event = get_event(dir)
+        event_reward_scalar = get_scalar_array(event, "Reward")
+        event_reward_avg = np.average(event_reward_scalar)
+        print("Average reward for " + os.path.basename(dir) + " " + str(event_reward_avg))
 
 def calc_event_reward_avg(event_file):
     ea = event_accumulator.EventAccumulator(event_file)
@@ -15,5 +25,36 @@ def calc_event_reward_avg(event_file):
          for scalar in ea.Scalars(scalar_str)])
     return np.average(lc)
 
+def get_event_dirs(exp_dir):
+    non_empty_dirs = []
+    for entry in os.listdir(exp_dir):
+        entry_path = os.path.join(exp_dir, entry)
+        if os.path.isdir(entry_path) and (len(os.listdir(entry_path)) > 0):
+            non_empty_dirs.append(entry_path)
+    return non_empty_dirs
+
+def summary_path(exp_name):
+    cur_dir = os.getcwd()
+    base_dir = os.path.dirname(cur_dir)
+    exp_summary_dir = os.path.join(base_dir, "experiments", exp_name, "summary")
+    if os.path.isdir(exp_summary_dir):
+        return exp_summary_dir
+    else:
+        raise Exception("'" + exp_summary_dir + "' is not a directory.")
+
+def get_event(dir_path):
+    return max(
+        glob.glob('{}/*'.format(dir_path)),
+        key=os.path.getctime)
+
+def get_scalar_array(event_file, scalar_str):
+    ea = event_accumulator.EventAccumulator(event_file)
+    ea.Reload()
+    lc = np.stack(
+        [np.asarray([scalar.step, scalar.value])
+         for scalar in ea.Scalars(scalar_str)])
+    return(lc)
+
 if __name__ == '__main__':
     main()
+
